b9c20a13d9df58ee0d6b262257196c9a6ad046c2
claudijo <claudijo.borovic@gmail.com>
2020-02-08 21:10:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Zmq probe
+
+Command line interface for [zmq-json-rpc-client](https://github.com/claudijo/zmq-json-rpc-client).
---
+++ bin/index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+const zmqJsonRpcClient = require('zmq-json-rpc-client');
+const util = require('util');
+
+const options = yargs
+  .usage('Usage: -n <name>')
+  .option('endpoint', { alias: 'e', describe: 'Zmq json rpc server endpoint or tcp port on localhost', type: 'string', demandOption: true })
+  .option('method', { alias: 'm', describe: 'A String containing the name of the method to be invoked.', type: 'string', demandOption: true })
+  .option('params', { alias: 'p', describe: 'A Structured value that holds the parameter values to be used during the invocation of the method. This member MAY be omitted.', type: 'string', demandOption: false })
+  .option('id', { alias: 'i', describe: 'An identifier established by the Client that MUST contain a String, Number, or NULL value if included. If it is not included it is assumed to be a notification.', type: 'sting', demandOption: false })
+  .argv;
+
+const endpoint = /^[0-9]+$/.test(options.endpoint) ? `tcp://127.0.0.1:${options.endpoint}` : options.endpoint;
+
+const client = zmqJsonRpcClient(endpoint, {
+  nextId: () => options.id,
+});
+
+const callback = typeof options.id === 'undefined' ? undefined : (err, result) => {
+  if (err) {
+    console.error(err);
+    return process.exit(1);
+  }
+
+  console.log(util.inspect(result, false, null, true));
+  process.exit(0);
+};
+
+const params = typeof options.params === 'undefined' ? undefined : JSON.parse(options.params);
+
+client.emit(options.method, params, callback);
+
+
