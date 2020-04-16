513943482b4f9ddc9123333ad274c3230cc2ba42
claudijo <claudijo.borovic@gmail.com>
2020-04-16 10:19:45 +0200

    Add request timeout option

--- bin/index.js
+++ bin/index.js
@@ -9,12 +9,14 @@ const options = yargs
   .option('method', { alias: 'm', describe: 'A String containing the name of the method to be invoked.', type: 'string', demandOption: true })
   .option('params', { alias: 'p', describe: 'A Structured value that holds the parameter values to be used during the invocation of the method. This member MAY be omitted.', type: 'string', demandOption: false })
   .option('id', { alias: 'i', describe: 'An identifier established by the Client that MUST contain a String, Number, or NULL value if included. If it is not included it is assumed to be a notification.', type: 'sting', demandOption: false })
+  .option('timeout', { alias: 't', describe: 'Request timeout in ms. Defaults to 30000 ms', type: 'number', demandOption: false })
   .argv;
 
 const endpoint = /^[0-9]+$/.test(options.endpoint) ? `tcp://127.0.0.1:${options.endpoint}` : options.endpoint;
 
 const client = zmqJsonRpcClient(endpoint, {
   nextId: () => options.id,
+  timeout: options.timeout,
 });
 
 const callback = typeof options.id === 'undefined' ? undefined : (err, result) => {
